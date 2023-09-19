7958d9ae36b738c6583e367a4968f61f7175bb69
jkeatas <jason.keatley@my.jcu.edu.au>
2023-09-19 12:35:46 +1000

    refactored temperatures added score.py to prac02

--- prac_02/score.py
+++ prac_02/score.py
@@ -3,14 +3,21 @@ CP1404/CP5632 - Practical
 Broken program to determine score status
 """
 
-# TODO: Fix this!
 
-score = float(input("Enter score: "))
-if score < 0 or score > 100:
-    print("Invalid score")
-elif score < 50:
-    print("Bad")
-elif 50 <= score < 90:
-    print("Passable")
-else:
-    print("Excellent")
+def main():
+    score = float(input("Enter score: "))
+    print(get_result(score))
+
+
+def get_result(score):
+    if score < 0 or score > 100:
+        return "Invalid score"
+    elif score < 50:
+        return "Bad"
+    elif 50 <= score < 90:
+        return "Passable"
+    else:
+        return "Excellent"
+
+
+main()
