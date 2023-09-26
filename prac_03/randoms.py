c63187059cdddc6b18a919c979dcebf3be1934ca
jkeatas <jason.keatley@my.jcu.edu.au>
2023-09-26 12:58:49 +1000

    added files

--- prac_03/randoms.py
+++ prac_03/randoms.py
@@ -1,5 +1,5 @@
 import random
-print(random.randint(5, 20))  # smallest 5, largest 20
-print(random.randrange(3, 10, 2))  # smallest 3, largest 9, no
-print(random.uniform(2.5, 5.5))  # smallest 2.5, largest 5.5
+print(random.randint(5, 20))  # 5, smallest 5, largest 20
+print(random.randrange(3, 10, 2))  # 9, smallest 3, largest 9, no
+print(random.uniform(2.5, 5.5))  # 4.961769915861707, smallest 2.5, largest 5.5
 print(random.randint(1, 100))
