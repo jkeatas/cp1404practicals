4fed166bb031750da4a406bec9190d1d86c49897
jkeatas <jason.keatley@my.jcu.edu.au>
2023-10-10 12:35:33 +1000

    edited emails

--- prac_05/emails.py
+++ prac_05/emails.py
@@ -3,18 +3,28 @@ Emails
 Estimate: 30 minutes
 Actual:   20 minutes
 """
-email = input("Email: ")
-print(email)
-name_and_email = {}
-while email != "":
+
+
+def main():
+    email = input("Email: ")
+    print(email)
+    name_and_email = {}
+    while email != "":
+        extract_name(email)
+        name_and_email[str(email).strip("[ ]' ' ")] = name
+        email = input("Email: ")
+    for item in name_and_email:
+        print(f"{name_and_email[item]} ({item})")
+
+
+def extract_name(email):
     name = email.replace('.', ' ').split("@")
     name.pop()
     name = str(name).strip("[ ]' ' ").title()
-    print(name)
     correct = input(f"Is your name {name} (Y/n): ")
     if correct != "" and correct.lower() != "y":
         name = input("Name: ").title()
-    name_and_email[str(email).strip("[ ]' ' ")] = name
-    email = input("Email: ")
-for item in name_and_email:
-    print(f"{name_and_email[item]} ({item})")
+    return name
+
+
+main()
