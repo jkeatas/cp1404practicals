8915d0c58d7bc5971b94fe80766b3a50bf170e85
Jason Keatley <jason.keatley@my.jcu.edu.au>
2023-11-01 22:13:55 +1000

    Complete More Guitars exercise

--- prac_07/myguitars.py
+++ prac_07/myguitars.py
@@ -11,7 +11,18 @@ for line in input_file:
     parts[1] = int(parts[1])
     parts[2] = float(parts[2])
     guitars.append(Guitar(parts[0], parts[1], parts[2]))
-
+name = input("Name: ")
+while name != "":
+    year = int(input("Year: "))
+    cost = int(input("Cost: "))
+    guitars.append(Guitar(name, year, cost))
+    print(f"{name} ({year}) : ${cost} added.")
+    name = input("Name: ")
+input_file.close()
+output_file = open(FILENAME, "w")
+for guitar in guitars:
+    output_file.write(str(guitar) + "\n")
+output_file.close()
 guitars.sort()
 for guitar in guitars:
     print(guitar)
