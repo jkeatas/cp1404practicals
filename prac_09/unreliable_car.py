f293a327cf1308533e863e48798d26f4e79f8df4
Jason Keatley <jason.keatley@my.jcu.edu.au>
2023-11-12 19:48:03 +1000

    completed unreliable car exercise

---
+++ prac_09/unreliable_car.py
@@ -0,0 +1,25 @@
+from prac_09.car import Car
+from random import randint
+
+class UnreliableCar(Car):
+
+    def __init__(self, name, fuel, reliability):
+        """Initialise a unreliable car instance, based on parent class Car."""
+        super().__init__(name, fuel)
+        self.reliability = reliability
+
+    def drive(self, distance):
+        """Drive the car a given distance.
+
+               Drive given distance if car has enough fuel and has a random number
+               higher than distance or drive until fuel runs out return the distance actually driven.
+               """
+        if distance > randint(0, 100):
+            distance = 0
+        elif distance > self.fuel:
+            distance = self.fuel
+            self.fuel = 0
+        else:
+            self.fuel -= distance
+        self.odometer += distance
+        return distance
